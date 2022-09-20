645943b0a3fab3d2f567ac73363c46e47a3f1228
killertofus <cinemassacres@gmail.com>
2022-09-20 05:05:40 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -9,7 +9,7 @@ wget https://installers.privateinternetaccess.com/download/pia-linux-3.3.1-06924
 chmod +x pia-linux-3.3.1-06924.run
 sh ./pia-linux-3.3.1-06924.run
 sudo systemctl enable sddm.service
-cd Config/
+cd Config
 mv Config/rc.lua ~/.config/awesome
 mv Config/rofi.rasi ~/.config/awesome/home/configuration
 cd 
