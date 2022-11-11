00edbe30197725037e83decb7bba29c731eff928
killertofus <cinemassacres@gmail.com>
2022-11-11 14:23:38 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -4,7 +4,7 @@ cd yay
 makepkg -si
 sudo pacman -Syu
 cd
-yay -S  rofi chatterino2-7tv-git vscodium git wget awesome sddm vlc librewolf caja streamlink streamlink-twitch-gui-bin qbittorrent jdownloader2 nitrogen rustdesk obs stacer gnome-disk-utility nano alacritty remmina p7zip-guifish flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji -y
+yay -S  rofi chatterino2-git vscodium git wget awesome sddm vlc librewolf caja streamlink streamlink-twitch-gui-bin qbittorrent jdownloader2 rustdesk obs gnome-disk-utility neovim alacritty remmina p7zip-gui zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji -y
 wget https://installers.privateinternetaccess.com/download/pia-linux-3.3.1-06924.run
 chmod +x pia-linux-3.3.1-06924.run
 sh ./pia-linux-3.3.1-06924.run
