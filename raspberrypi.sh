5a7fa2e3fd62983f7c5611fe7e18e9c03fd1305c
killertofus <cinemassacres@gmail.com>
2022-11-15 19:55:55 +0000

    Update raspberrypi.sh

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -1,5 +1,5 @@
 sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
-sudo apt-get install lightdm x11vnc nvim git kodi zsh                -y
+sudo apt-get install lightdm x11vnc nvim git kodi zsh -y
 sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 echo 'deb http://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/Raspbian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
 curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/Raspbian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
@@ -9,7 +9,7 @@ cd zsh
 mv dracula.zsh-theme lib ..
 sudo apt update
 sudo apt install zsh-autosuggestions -y
-sudo apt remove libreoffice gnome-games -y 
+sudo apt remove libreoffice gnome-games nano -y 
 sudo snap remove firefox -y
 sudo add-apt-repository ppa:mozillateam/ppa -y
 sudo apt remove --autoremove firefox -y
