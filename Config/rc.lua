4b6da5c6a4532fa3339b76205fe1b5bc9fcbea2d
killertofus <cinemassacres@gmail.com>
2022-12-18 19:10:09 -0700

    Update rc.lua

--- Config/rc.lua
+++ Config/rc.lua
@@ -1,3 +1,4 @@
+-- https://github.com/frapdotbmp/dotfiles :)
 -- If LuaRocks is installed, make sure that packages installed through it are
 -- found (e.g. lgi). If LuaRocks is not installed, do nothing.
 pcall(require, "luarocks.loader")
