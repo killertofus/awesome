4202c969acbd9419807c6456da6648700e43e043
killertofus <cinemassacres@gmail.com>
2023-07-11 18:08:03 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -5,11 +5,12 @@ makepkg -si
 yay --devel --save
 sudo pacman -Syu
 cd
-yay -S  rofi chatterino2-git ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd -y
+yay -S  rofi chatterino2-git ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility ttf-nerd-fonts-symbols neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd  -y
 cd Config
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 mv rc.lua ~/.config/awesome
 mv rofi.rasi ~/.config/awesome/home/configuration
 cd 
 mv Wallpaper.jpg ~/Pictures
-RANGER_LOAD_DEFAULT_RC=false
+echo "RANGER_LOAD_DEFAULT_RC=false"
+git clone https://github.com/alexanderjeurissen/ranger_devicons ~/.config/ranger/plugins/ranger_devicons
