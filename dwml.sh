4eefdedb18c478c5bc67858c9ade0f5289b61d3e
killertofus <cinemassacres@gmail.com>
2023-08-09 22:32:22 -0600

    see above

--- dwml.sh
+++ dwml.sh
@@ -1,5 +1,5 @@
 apt install xdotool
-xdot key control alt f5
+xdottool key control alt f5
 kill echo $XDG_CURRENT_DESKTOP
  apt purge echo $XDG_CURRENT_DESKTOP -y
  #remove below when alacritty is in the mint repos
