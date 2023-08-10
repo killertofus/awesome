7c88d4ed2acbfabe382ea13cd37c4b9d1c679712
killertofus <cinemassacres@gmail.com>
2023-08-09 22:38:22 -0600

    needed to be capital

--- dwml.sh
+++ dwml.sh
@@ -1,5 +1,5 @@
 apt install xdotool
-xdotool key control alt f5
+xdotool key control alt F5
 kill echo $XDG_CURRENT_DESKTOP
  apt purge echo $XDG_CURRENT_DESKTOP -y
  #remove below when alacritty is in the mint repos
