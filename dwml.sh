4b7d8a0a4d086c916a059e687d93b98f7843bc70
killertofus <cinemassacres@gmail.com>
2023-08-10 10:34:05 -0600

    made the config folder if not already there

--- dwml.sh
+++ dwml.sh
@@ -58,9 +58,16 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 znap pull
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+mkdir -p ~/.config/nvim
+mkdir -p ~/.config/alacritty
+mkdir -p ~/.config/awesome
+mkdir -p ~/.config/rofi
+mkdir -p ~/.config/picom
+
 mv init.vim ~/.config/nvim/
-mv alacritty.yml ~/.config/alacritty/alacritty.yml
+mv alacritty.yml ~/.config/alacritty/
 mv rc.lua ~/.config/awesome
-mv rofi.rasi ~/.config/awesome/home/configuration
+mv rofi.rasi ~/.config/rofi
+mv picom.conf ~/.config/picom
 cd 
 echo "RANGER_LOAD_DEFAULT_RC=false"
