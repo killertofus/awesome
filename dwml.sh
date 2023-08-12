74961d63d50a8b75ab36edeb93c7af9b638cef8c
killertofus <cinemassacres@gmail.com>
2023-08-12 17:04:48 -0600

    now correctly moves the files

--- dwml.sh
+++ dwml.sh
@@ -70,6 +70,7 @@ git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-m
 znap pull
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+cd config
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
 mkdir -p ~/.config/awesome
