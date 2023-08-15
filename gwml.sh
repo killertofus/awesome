950eca0a1121951ef160904644ef4141ba8a2612
killertofus <cinemassacres@gmail.com>
2023-08-15 03:37:39 +0000

    Update and rename zwml.sh to gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -1,5 +1,10 @@
 #!usr/bin/zsh
+
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv /lib dracula.zsh-theme ~/oh-my-zsh/themes/lib
