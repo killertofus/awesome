19c61dcf426fd86f07d986568cc548a574cd389e
killertofus <cinemassacres@gmail.com>
2023-08-15 03:43:37 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -1,5 +1,4 @@
 #!usr/bin/zsh
-
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
