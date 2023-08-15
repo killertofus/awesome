a558cc785ea0da0b26b6230547f1949efec5c656
killertofus <cinemassacres@gmail.com>
2023-08-15 01:06:15 +0000

    Update zwml.sh

--- zwml.sh
+++ zwml.sh
@@ -1,3 +1,6 @@
 #!usr/bin/zsh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --keep-zshrc
+wget https://github.com/dracula/zsh/archive/master.zip
+unzip master.zip
+mv /lib dracula.zsh-theme ~/oh-my-zsh/themes/lib
 znap pull
