749da3416654ba4198a866acb7b1560e4c4f33d7
killertofus <cinemassacres@gmail.com>
2023-08-15 18:33:04 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -7,6 +7,7 @@ xargs sudo dpkg --purge <dwmlrmvpkgs.txt
  sudo apt update
  sudo apt install alacritty
 curl -sS https://webi.sh/lsd | sh
+source ~/config/envman/PATH.env
 distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
 
 wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
