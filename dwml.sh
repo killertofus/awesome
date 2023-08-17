e012d2784d86f833a439effcf8e6267dd0bf0734
killertofus <cinemassacres@gmail.com>
2023-08-17 21:43:29 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -60,6 +60,7 @@ curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest \
 
 
 cd -
+pwd
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 sudo apt update
