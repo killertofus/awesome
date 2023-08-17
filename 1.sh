cacbf269cd287d84034733578bae36f768996f04
killertofus <cinemassacres@gmail.com>
2023-08-17 20:54:45 +0000

    Create 1.sh

---
+++ 1.sh
@@ -0,0 +1,9 @@
+pkgToRemoveListFull="emacsen-common dictionaries-comon $DESKTOP_SESSION gnome-games hypnotix hexchat libreoffice-base-core p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny warpinator youtube-dl yelp"
+pkgToRemoveList=""
+for pkgToRemove in $(echo $pkgToRemoveListFull); do
+  $(dpkg --status $pkgToRemove &> /dev/null)
+  if [[ $? -eq 0 ]]; then
+    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
+  fi
+done
+apt-get --yes --purge remove $pkgToRemoveList
