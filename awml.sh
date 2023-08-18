22fee00697ff94da71171903de2b18c76ccaf216
killertofus <cinemassacres@gmail.com>
2023-08-18 21:35:04 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -7,4 +7,5 @@ sudo pacman -Syu
 cd -
 yay -S  rofi chatterino2-git ttf-hack ttf-noto-fonts-ib picom ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility ttf-nerd-fonts-symbols neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd  -y
 ./gwml.sh
+chsh -s $(which zsh)
 echo "RANGER_LOAD_DEFAULT_RC=false"
