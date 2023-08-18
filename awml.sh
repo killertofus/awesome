2062932d28112ec5ce4d1f7ea04c3b173907abe5
killertofus <cinemassacres@gmail.com>
2023-08-18 21:36:57 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -1,11 +1,12 @@
-pacman -S --needed git base-devel
+sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 yay --devel --save
 sudo pacman -Syu
 cd -
-yay -S  rofi chatterino2-git ttf-hack ttf-noto-fonts-ib picom ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility ttf-nerd-fonts-symbols neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd  -y
+sudo pacman -S  rofi chatterino2-git ttf-hack ttf-noto-fonts-ib picom ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility ttf-nerd-fonts-symbols neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd  -y
+yay -S chatterino2-git
 ./gwml.sh
 chsh -s $(which zsh)
 echo "RANGER_LOAD_DEFAULT_RC=false"
