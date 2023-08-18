b8ba960119e224b5eef56077064d2b930436837c
killertofus <cinemassacres@gmail.com>
2023-08-18 02:00:16 +0000

    Update dwmlrmvpkgs.sh

--- dwmlrmvpkgs.sh
+++ dwmlrmvpkgs.sh
@@ -1,4 +1,4 @@
-pkgToRemoveListFull=" emacsen-common dictionaries-common $DESKTOP_SESSION gnome-games *yaru* gnome-terminal  orca xterm *transmission* hypnotix hexchat yelp-xsl *libreoffice* p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny warpinator youtube-dl yelp *gnome*"
+pkgToRemoveListFull=" emacsen-common dictionaries-common $DESKTOP_SESSION  *nemo* gnome-games *yaru* gnome-terminal  orca xterm *transmission* hypnotix hexchat yelp-xsl *libreoffice* p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny warpinator youtube-dl yelp *gnome*"
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
