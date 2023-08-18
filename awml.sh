99c97e6372c4802cb629825dce0fe6dbfa812e1a
killertofus <cinemassacres@gmail.com>
2023-08-18 21:34:26 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -4,12 +4,7 @@ cd yay
 makepkg -si
 yay --devel --save
 sudo pacman -Syu
-cd
+cd -
 yay -S  rofi chatterino2-git ttf-hack ttf-noto-fonts-ib picom ranger git piavpn-bin wget awesome w3m vlc librewolf caja streamlink python-libtmux tmux streamlink-twitch-gui-bin qbittorrent jdownloader2 ripgrep rustdesk obs gnome-disk-utility ttf-nerd-fonts-symbols neovim alacritty sftpman-gtk xarchiver unzip zsh flameshot github-desktop-bin timeshift noto-fonts-cjk noto-fonts-emoji lsd  -y
-cd Config
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-mv rc.lua ~/.config/awesome
-mv rofi.rasi ~/.config/awesome/home/configuration
-cd 
-mv Wallpaper.jpg ~/Pictures
+./gwml.sh
 echo "RANGER_LOAD_DEFAULT_RC=false"
