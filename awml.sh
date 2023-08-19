4a807cbe4ca013f63fe228376540ccbb6ff0b4c2
killertofus <cinemassacres@gmail.com>
2023-08-19 03:20:24 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -2,8 +2,8 @@ sudo pacman -S --needed git base-devel
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
-yay -S <ywmlpkgs.txt -y
-sudo pacman -S <awmlpkgs.txt -y
+yay -S < ywmlpkgs.txt -y
+sudo pacman -S < awmlpkgs.txt -y
 yay --devel --save
 sudo pacman -Syu
 cd -
