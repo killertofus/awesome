5b4e0b8cfcd972dc3d56a0020af320eaf07f7ea9
killertofus <cinemassacres@gmail.com>
2023-08-21 03:59:50 +0000

    Update dwmlrmvpkgs.sh

--- dwmlrmvpkgs.sh
+++ dwmlrmvpkgs.sh
@@ -9,13 +9,3 @@ for pkgToRemove in $(echo $pkgToRemoveListFull); do
 done
 apt --yes --purge remove $pkgToRemoveList
 else
-if [  -n "$(uname -a | grep kubuntu)" ]; then
-pkgToRemoveListFull= " yt-dlp vim-common libreoffice-base-core $DESKTOP_SESSION nano "
-pkgToRemoveList=""
-for pkgToRemove in $(echo $pkgToRemoveListFull); do
-  $(dpkg --status $pkgToRemove &> /dev/null)
-  if [[ $? -eq 0 ]]; then
-    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
-  fi
-done
-apt --yes --purge remove $pkgToRemoveList
