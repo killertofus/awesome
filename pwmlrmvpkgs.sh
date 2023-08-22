320af9fe0e5c84f3e9810c80fde77bcafb379815
killertofus <cinemassacres@gmail.com>
2023-08-22 19:57:21 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,1 +1,9 @@
-sudo apt purge yelp vim-common vim-tiny libreoffice-base-core $DESKTOP_SESSION  'language-pack.*$' nano pop-desktop --allow-remove-essential
+pkgToRemoveListFull="  yelp vim-common vim-tiny libreoffice-base-core $DESKTOP_SESSION  'language-pack.*$' nano pop-desktop --allow-remove-essential
+pkgToRemoveList=""
+for pkgToRemove in $(echo $pkgToRemoveListFull); do
+  $(dpkg --status $pkgToRemove &> /dev/null)
+  if [[ $? -eq 0 ]]; then
+    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
+  fi
+done
+apt --yes --purge remove $pkgToRemoveList
