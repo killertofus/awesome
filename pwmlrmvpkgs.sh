f66ce8bd0b79100a74d94a181af193c47ffdd37e
killertofus <cinemassacres@gmail.com>
2023-08-22 01:34:35 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,7 +1,6 @@
-pkgToRemoveListFull= " *$DESKTOP_SESSION* gnome-games gnome-terminal orca xterm libreoffice-base-core snapd snap vim-common vim-tiny yelp *gnome*"
-pkgToRemoveList=""
-for pkgToRemove in $(echo $pkgToRemoveListFull); do
-  $(dpkg --status $pkgToRemove &> /dev/null)
+pkgToRemoveList="*$DESKTOP_SESSION* gnome-games gnome-terminal orca xterm libreoffice-base-core snapd snap vim-common vim-tiny yelp *gnome* "
+for pkgToRemove in $(echo $pkgToRemoveList); do
+  $(dpkg --status $pkgToRemoveList &> /dev/null)
   if [[ $? -eq 0 ]]; then
     pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
   fi
