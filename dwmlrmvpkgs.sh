360b23f7c2a9ad4d1ac91cf31b65c3ea4a201d1c
killertofus <cinemassacres@gmail.com>
2023-08-22 00:52:18 +0000

    Update dwmlrmvpkgs.sh

--- dwmlrmvpkgs.sh
+++ dwmlrmvpkgs.sh
@@ -1,4 +1,3 @@
-if [  -n "$(uname -a | grep mint)" ]; then
 pkgToRemoveListFull=" emacsen-common dictionaries-common gedit celluoid *$DESKTOP_SESSION* gnome-games gnome-terminal hypnotix hexchat orca xterm transmission libreoffice-base-core p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny warpinator youtube-dl yelp *gnome*"
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
