c3b32997a87ec31a89c98b1726ec356f76901f14
killertofus <cinemassacres@gmail.com>
2023-08-22 01:52:36 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,9 +1,1 @@
-pkgToRemoveListFull= " emacsen-common dictionaries-common gedit celluoid *$DESKTOP_SESSION* gnome-games gnome-terminal hypnotix hexchat orca xterm transmission libreoffice-base-core p7zip-full rhythmbox-tray-icon rhythmbox-data rhythmbox-plugins snapd snap rhythmbox vim-common vim-tiny warpinator youtube-dl yelp *gnome*"
-pkgToRemoveList=""
-for pkgToRemove in $(echo $pkgToRemoveListFull); do
-  $(dpkg --status $pkgToRemove &> /dev/null)
-  if [[ $? -eq 0 ]]; then
-    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
-  fi
-done
-apt --yes --purge remove $pkgToRemoveList
+sudo apt purge yelp vim-common vim-tiny libreoffice-base-core $DESKTOP_SESSION  'language-pack.*$' nano
