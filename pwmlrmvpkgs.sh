c34ef87f360a62d2d003bee441bcbede469bec7e
killertofus <cinemassacres@gmail.com>
2023-08-24 03:51:27 +0000

    moved to pop-os branch

--- pwmlrmvpkgs.sh
+++
@@ -1,9 +0,0 @@
-pkgToRemoveListFull= " yelp vim-common vim-tiny libreoffice-base-core $DESKTOP_SESSION  'language-pack.*$' nano "
-pkgToRemoveList=""
-for pkgToRemove in $(echo $pkgToRemoveListFull); do
-  $(dpkg --status $pkgToRemove &> /dev/null)
-  if [[ $? -eq 0 ]]; then
-    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
-  fi
-done
-apt --yes --purge remove $pkgToRemoveList
