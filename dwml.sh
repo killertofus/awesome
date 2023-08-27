1968481a237488cc7332a58cd5212103b79b5373
killertofus <cinemassacres@gmail.com>
2023-08-27 19:55:49 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -32,22 +32,6 @@ Signed-By: /usr/share/keyrings/librewolf.gpg
 EOF
 sudo apt update
 
-curl https://repo.jellyfin.org/install-debuntu.sh | sudo bash
-sudo mkdir -p /etc/apt/keyrings
-curl -fsSL https://repo.jellyfin.org/jellyfin_team.gpg.key | sudo gpg --dearmor -o /etc/apt/keyrings/jellyfin.gpg
-cat <<EOF | sudo tee /etc/apt/sources.list.d/jellyfin.sources
-Types: deb
-URIs: https://repo.jellyfin.org/$( awk -F'=' '/^ID=/{ print $NF }' /etc/os-release )
-Suites: $( awk -F'=' '/^VERSION_CODENAME=/{ print $NF }' /etc/os-release )
-Components: main
-Architectures: $( dpkg --print-architecture )
-Signed-By: /etc/apt/keyrings/jellyfin.gpg
-EOF
-sudo apt update
-sudo apt install jellyfin
-sudo systemctl {action} jellyfin
-
-
 
 sudo apt install librewolf -y
 sudo mv streamlink.desktop /usr/share/applications
