d56f5160e2de01a355b4237a6bb335783429f17f
killertofus <cinemassacres@gmail.com>
2023-08-28 02:58:40 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,4 +1,4 @@
-pkgToRemoveListFull=" yelp vim-common vim-tiny baobab gnome-online-miners gnome-calculator gnome-settings-daemon-common gnome-menus geary orca gnome-desktop3-data gedit-common language-selector-gnome gnome-shell-common printer-driver-all toilet pop-server libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit pop-desktop pop-shell pop-cosmic gnome-terminal hunspell hyphen grim zenity "
+pkgToRemoveListFull=" yelp vim-common vim-tiny baobab gnome-online-miners gnome-bluetooth-common gnome-calculator gnome-settings-daemon-common gnome-menus geary orca gnome-desktop3-data gedit-common language-selector-gnome gnome-shell-common printer-driver-all toilet pop-server libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit pop-desktop pop-shell pop-cosmic gnome-terminal hunspell hyphen grim zenity "
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
