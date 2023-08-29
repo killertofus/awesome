3cc82c2d1f1217e917b78d1d802a79cf18136ea6
killertofus <cinemassacres@gmail.com>
2023-08-29 00:37:25 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,4 +1,4 @@
-pkgToRemoveListFull="  printer-driver-all yelp vim-common vim-tiny baobab gnome-online-miners gnome-bluetooth-common pop-desktop pop-shell gnome-calculator gnome-settings-daemon-common geary orca gedit-common language-selector-gnome gnome-shell-common  toilet libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit gnome-terminal hunspell hyphen grim zenity  "
+pkgToRemoveListFull="  yelp vim-common vim-tiny baobab gnome-online-miners gnome-bluetooth-common pop-desktop pop-shell gnome-calculator gnome-settings-daemon-common geary orca gedit-common language-selector-gnome gnome-shell-common  toilet libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit gnome-terminal hunspell hyphen grim zenity printer-driver-all   "
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
@@ -6,4 +6,4 @@ for pkgToRemove in $(echo $pkgToRemoveListFull); do
     pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
   fi
 done
-sudo apt --yes --purge remove --allow-remove-essential $pkgToRemoveList -y
+sudo apt --yes --purge remove --allow-remove-essential -y $pkgToRemoveList -y
