86c0d1e21f49a54678d239ae692c70853cfd2095
killertofus <cinemassacres@gmail.com>
2023-08-31 20:20:13 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,3 +1,4 @@
+# please don't run this more then twice :)
 pkgToRemoveListFull="  yelp vim-common vim-tiny baobab gnome-online-miners gnome-bluetooth-common pop-desktop pop-shell gnome-calculator gnome-settings-daemon-common geary orca gedit-common language-selector-gnome gnome-shell-common  toilet libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit gnome-terminal hunspell hyphen grim zenity  "
 pkgToRemoveList=""
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
@@ -6,4 +7,4 @@ for pkgToRemove in $(echo $pkgToRemoveListFull); do
     pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
   fi
 done
-sudo apt --yes --purge remove --allow-remove-essential -y $pkgToRemoveList -y
+apt --yes --purge remove --allow-remove-essential $pkgToRemoveList
