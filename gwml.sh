7991feab1c88ac6b0980ccd1ce84c8372db199b8
killertofus <cinemassacres@gmail.com>
2023-09-29 03:27:02 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -15,7 +15,7 @@ mkdir -p ~/.config/ranger
 mkdir -p ~/.config/tmux
 mv config/init.vim ~/.config/nvim/
 mv config/alacritty.yml ~/.config/alacritty
-mv config/rc.lua theme ~/.config/dwm
+mv config/patch ~/.config/dwm
 mv config/config1.rasi ~/.config/rofi
 mv config/picom.conf ~/.config/picom
 mv config/tmux.conf ~/.config/tmux
