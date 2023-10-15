9d930526574edbc51f9953b9ee5d3d1fb51a38d7
killertofus <cinemassacres@gmail.com>
2023-10-15 15:41:40 +0000

    Update config.h

--- config/dwm/config.h
+++ config/dwm/config.h
@@ -48,6 +48,7 @@ static const Layout layouts[] = {
 
 /* key definitions */
 #define MODKEY Mod1Mask
+#define MOD2KEY Mod4Mask
 #define TAGKEYS(KEY,TAG) \
 	{ MODKEY,                       KEY,      view,           {.ui = 1 << TAG} }, \
 	{ MODKEY|ControlMask,           KEY,      toggleview,     {.ui = 1 << TAG} }, \
@@ -66,7 +67,7 @@ static const char *termcmd[]  = { "st", NULL };
 static const Key keys[] = {
 	/* modifier                     key        function        argument */
 	{ MODKEY,                       XK_s,      togglealwaysontop, {0} },
-	{ MODKEY,                       XK_d,      spawn,          {.v = roficmd } },
+	{ MOD2KEY,                      0,         spawn,          {.v = roficmd } },
 	{ MODKEY,                       XK_p,      spawn,          {.v = dmenucmd } },
 	{ MODKEY|ShiftMask,             XK_Return, spawn,          {.v = termcmd } },
 	{ MODKEY,                       XK_b,      togglebar,      {0} },
