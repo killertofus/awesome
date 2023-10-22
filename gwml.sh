55c3f7aa3e71e78ad68372308bf397dc7ba281d9
killertofus <cinemassacres@gmail.com>
2023-10-22 23:08:13 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -29,7 +29,6 @@ wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv lib dracula.zsh-theme ~/oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-sudo ln -sf ~/.config/dwm/slstatus /usr/local/bin/
 make -C ~/.config/dwm clean
 make -C ~/.config/dwm/slstatus clean
 sudo make -C ~/.config/dwm/ clean install
