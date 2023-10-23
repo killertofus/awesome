921cdae481d0770ac880d7272f5faf7c135ad154
killertofus <cinemassacres@gmail.com>
2023-10-23 22:07:27 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -20,6 +20,7 @@ mv config/rc.lua theme ~/.config/awesome
 mv config/config1.rasi ~/.config/rofi
 mv config/picom.conf ~/.config/picom
 mv config/tmux.conf ~/.config/tmux
+mv config/.zshrc ~/
 mv config/plugins config/rc.conf ~/.config/ranger
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
