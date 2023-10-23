e661e00c781d8c5b6eb3e434dc3d7fbe1157fd9e
killertofus <cinemassacres@gmail.com>
2023-10-23 20:58:12 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -14,6 +14,7 @@ mkdir -p ~/.config/picom
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/tmux
 mv config/init.vim ~/.config/nvim/
+mv config/.xinitrc ~/
 mv config/alacritty.yml ~/.config/alacritty
 mv config/rc.lua theme ~/.config/awesome
 mv config/config1.rasi ~/.config/rofi
