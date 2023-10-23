ecc8fc4a526d007a7b0b14566e3573653d0cdd94
killertofus <cinemassacres@gmail.com>
2023-10-23 20:22:06 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -21,7 +21,7 @@ mv config1.rasi ~/.config/rofi
 mv picom.conf ~/.config/picom
 mv tmux.conf ~/.config/tmux
 mv .dircolors ~/
-mv plugins rc.conf ~/.config/ranger
+mv config/plugins config/rc.conf ~/.config/ranger
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
