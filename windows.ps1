9956d0f7fe64b9113a2cb25bec35f663fd73c362
killertofus <cinemassacres@gmail.com>
2023-10-31 20:53:25 +0000

    Update and rename windows.psi to windows.ps1

--- windows.ps1
+++ windows.ps1
@@ -1,4 +1,4 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
-choco install packages.config
+choco install packages.config -y
 
 irm https://christitus.com/win | iex
