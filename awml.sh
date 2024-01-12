d82e79fa3a9e91f3eef056393b169c8a33770bd6
killertofus <cinemassacres@gmail.com>
2024-01-12 19:07:37 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -4,6 +4,8 @@ git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si
 cd -
+sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
+sed -i '/color/s/^#//g' /etc/pacman.conf
 for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
 sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1)
 sudo systemctl enable ly
