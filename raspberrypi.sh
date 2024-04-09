cc5163f528766d628375d2b20bce3bf38a29278f
killertofus <cinemassacres@gmail.com>
2024-04-09 17:28:07 +0000

    Update raspberrypi.sh

--- raspberrypi.sh
+++ raspberrypi.sh
@@ -1,30 +1,71 @@
-sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
-sudo apt-get install lightdm x11vnc nvim git kodi zsh -y
-wget https://installers.privateinternetaccess.com/download/pia-linux-arm64-3.3.1-06924.run
-sh ./pia-linux-arm64-3.3.1-06924.run
-sh -c "$(wget -O- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-echo 'deb https://download.opensuse.org/repositories/shells:/zsh-users:/zsh-autosuggestions/Raspbian_11/ /' | sudo tee /etc/apt/sources.list.d/shells:zsh-users:zsh-autosuggestions.list
-curl -fsSL https://download.opensuse.org/repositories/shells:zsh-users:zsh-autosuggestions/Raspbian_11/Release.key | gpg --dearmor | sudo tee /etc/apt/trusted.gpg.d/shells_zsh-users_zsh-autosuggestions.gpg > /dev/null
-cd .oh-my-zsh/themes/
-git clone https://github.com/dracula/zsh.git
-cd zsh 
-mv dracula.zsh-theme lib ..
-sudo apt update
-sudo apt install zsh-autosuggestions -y
-sudo apt remove libreoffice gnome-games nano snap -y 
-sudo snap remove firefox
-sudo add-apt-repository ppa:mozillateam/ppa -y
-sudo apt remove --autoremove firefox -y
-echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
-sudo apt install firefox -y
-nvim /etc/apt/preferences.d/99mozillateamppa
-sudo rm -rf /var/cache/snapd/
-sudo apt install -t 'o=LP-PPA-mozillateam' firefox -y
-sudo apt autoremove --purge snapd gnome-software-plugin-snap -y
+#!/usr/bin/env bash
+sudo rm -rf */usr/share/xsessions
+sudo wget -p /usr/share/fonts/Iosevka
+sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
+sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevka/
+sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
+xargs sudo apt install <dwmlpkgs.txt
+fc-cache -f -v
+ #remove below when lsd is in the popos repo
+sudo dpkg --add-architecture i386
+sudo mkdir -pm755 /etc/apt/keyrings
+sudo wget -O /etc/apt/keyrings/winehq-archive.key https://dl.winehq.org/wine-builds/winehq.key
+main() {
+   codename=$(grep -m1 "CODENAME" /etc/os-release | cut -d'=' -f2)
+    printf "%b" '\033[1;33mDownloading..\n\033[0m'
+    sudo wget -NP /etc/apt/sources.list.d/ https://dl.winehq.org/wine-builds/ubuntu/dists/$codename/winehq-$codename.sources
+    printf "%b" '\033[1;32mDone! Goodbye!\n\033[0m'
+}
+
+main
+sudo apt update && sudo apt upgrade -y
+sudo apt install winehq-staging -y 
+source ~/config/envman/PATH.env
 rm -rf ~/snap
-nvim /lib/systemd/system/x11vnc.service
-systemctl daemon-reload
-systemctl enable x11vnc.service
-systemctl start x11vnc.service
-systemctl status x11vnc.service
-sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
+distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+
+rm -rf ~/snap
+distro=$(if echo " una bookworm vanessa focal jammy bullseye vera uma " | grep -q " $(lsb_release -sc) "; then echo $(lsb_release -sc); else echo focal; fi)
+
+wget -O- https://deb.librewolf.net/keyring.gpg | sudo gpg --dearmor -o /usr/share/keyrings/librewolf.gpg
+
+sudo tee /etc/apt/sources.list.d/librewolf.sources << EOF > /dev/null
+Types: deb
+URIs: https://deb.librewolf.net
+Suites: $distro
+Components: main
+Architectures: amd64
+Signed-By: /usr/share/keyrings/librewolf.gpg
+EOF
+sudo apt update
+
+sudo apt install librewolf -y
+sudo apt update
+
+sudo mv rustdesk.desktop /usr/share/applications
+sudo mv *.png /usr/share/icons
+sudo mkdir -p /usr/local/bin
+
+curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest \
+| grep "x86_64.*AppImage" \
+| cut -d : -f 2,3 \
+| tr -d \" \
+| wget -qi -
+ 
+ mv *.AppImage rustdesk
+ find ./  -regextype posix-egrep -regex '.*{3,5}.*' -print0 | xargs -0 chmod +x
+sudo mv rustdesk  /usr/local/bin
+
+
+sudo apt update -y && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
+./gwml.sh
+mv config/.zshrc ~/.
+./pwmlrmvpkgs.sh
+git clone --recurse-submodules https://github.com/fairyglade/ly
+cd ly
+make
+sudo make install installsystemd
+sudo systemctl enable ly.service -f
+sudo systemctl disable getty@tty2.service
+sudo apt update -y && sudo apt upgrade -y && sudo apt clean -y && sudo apt autoclean -y && sudo apt autoremove -y
+chsh -s $(which zsh)
