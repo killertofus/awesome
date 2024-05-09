2493b0116aee7af16ed499718ac959ff26febe5f
killertofus <cinemassacres@gmail.com>
2024-05-09 18:00:04 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -30,4 +30,4 @@ wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-./.tmux/plugins/tpm/scripts/install_plugins.sh
+./~/.tmux/plugins/tpm/scripts/install_plugins.sh
