63f95a374d286a4a1fce02076e2089be2b9c803d
killertofus <cinemassacres@gmail.com>
2024-05-09 04:21:16 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -28,5 +28,6 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv zsh-master/lib zsh-master/dracula.zsh-theme ~/oh-my-zsh/themes
+mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+./.tmux/plugins/tpm/scripts/install_plugins.sh
