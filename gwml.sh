aa42a9aeeee27ad3f0830c1f69d07ba0d513914d
killertofus <cinemassacres@gmail.com>
2024-05-10 02:49:41 +0000

    Update gwml.sh

--- gwml.sh
+++ gwml.sh
@@ -8,31 +8,26 @@ git clone https://github.com/dracula/gtk
 mv gtk/gtk-4.0 ~/.themes
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/dwm
+mkdir -p ~/.config/awesome
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/picom
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/tmux
-mv config/.xinitrc ~/
-sudo mv config/dwm.desktop /usr/share/xsessions
 mv config/init.vim ~/.config/nvim/
+mv config/.xinitrc ~/
 mv config/alacritty.toml ~/.config/alacritty
-mv config/dwm/* ~/.config/dwm
+mv config/rc.lua config/theme ~/.config/awesome
 mv config/config.rasi ~/.config/rofi
 mv config/picom.conf ~/.config/picom
 mv config/tmux.conf ~/.config/tmux
-mv config/plugins config/rc.conf ~/.config/ranger
-mv config/.dircolors ~/
 mv config/.zshrc ~/
+mv config/plugins config/rc.conf ~/.config/ranger
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv config/zsh-master lib dracula.zsh-theme ~/oh-my-zsh/themes
+mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-make -C ~/.config/dwm clean
-make -C ~/.config/dwm/slstatus clean
-sudo make -C ~/.config/dwm/ clean install
-sudo make -C ~/.config/dwm/slstatus clean install
+~/.tmux/plugins/tpm/scripts/install_plugins.sh
