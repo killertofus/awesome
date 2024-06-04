91672286902d739a4e70ec9cbf51246b4bd0f8ea
killertofus <cinemassacres@gmail.com>
2024-06-04 17:24:29 +0000

    Update gwml.sh
    tell a friend

--- gwml.sh
+++ gwml.sh
@@ -8,20 +8,23 @@ git clone https://github.com/dracula/gtk
 mv gtk/gtk-4.0 ~/.themes
 mkdir -p ~/.config/nvim
 mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/awesome
+mkdir -p ~/.config/dwm
 mkdir -p ~/.config/rofi
 mkdir -p ~/.config/picom
 mkdir -p ~/.config/ranger
 mkdir -p ~/.config/tmux
-mv config/init.vim ~/.config/nvim/
+mv wallpaper.jpg ~/.config/dwm/wallpaper.jpg
+sudo mv config/dwm.desktop /usr/share/xsessions/
 mv config/.xinitrc ~/
+mv config/init.vim ~/.config/nvim/
 mv config/alacritty.toml ~/.config/alacritty
-mv config/rc.lua config/theme ~/.config/awesome
+mv config/dwm/* ~/.config/dwm
 mv config/config.rasi ~/.config/rofi
 mv config/picom.conf ~/.config/picom
 mv config/tmux.conf ~/.config/tmux
 mv config/.zshrc ~/
 mv config/plugins config/rc.conf ~/.config/ranger
+mv config/.dircolors ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
@@ -29,5 +32,10 @@ sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
+rm -rf master.zip
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.tmux/plugins/tpm/scripts/install_plugins.sh
+make -C ~/.config/dwm/ clean
+sudo make -C ~/.config/dwm/ clean install
+make -C ~/.config/dwm/slstatus/ clean
+sudo make -C ~/.config/dwm/slstatus/ clean install
