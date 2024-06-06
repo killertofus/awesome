3600eff4e08341080a1b816a83cbf96a2c56fcd3
killertofus <cinemassacres@gmail.com>
2024-06-06 10:39:35 -0600

    Revert "Merge remote-tracking branch 'origin/main' into pop-os"
    This reverts commit f98579e4da691add39a65cccfab9b9997336242a, reversing
    changes made to 42548891504bd4b8f7edf2f4f0f810e65c820056.

---
+++ pwmlrmvpkgs.sh
@@ -0,0 +1,10 @@
+# please don't run this more then twice :)
+pkgToRemoveListFull="  yelp vim-common vim-tiny baobab gnome-online-miners gnome-bluetooth-common gnome-calculator gnome-terminal gnome-settings-daemon-common geary orca gedit-common language-selector-gnome toilet libreoffice-common libreoffice-base-core $DESKTOP_SESSION 'language-pack*' nano gedit gnome-terminal hunspell hyphen grim zenity gnome-control-center libreoffice-core  pop-de-gnome libreoffice-gnome pop-desktop *libreoffice* *gnome* "
+pkgToRemoveList=""
+for pkgToRemove in $(echo $pkgToRemoveListFull); do
+  $(dpkg --status $pkgToRemove &> /dev/null)
+  if [[ $? -eq 0 ]]; then
+    pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
+  fi
+done
+sudo dpkg --remove --force-all $pkgToRemoveList
