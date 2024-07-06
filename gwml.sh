4eeff3a081602c2f37daf33538e0812150d25d0c
killertofus <cinemassacres@gmail.com>
2024-07-06 14:08:31 -0600

    That

--- gwml.sh
+++ gwml.sh
@@ -1,14 +1,9 @@
 #!/usr/bin/env sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mkdir -p ~/.themes
+mv .themes ~/
 git clone https://github.com/dracula/gtk
-mv gtk/gtk-4.0 ~/.themes
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/alacritty
-mv config/.xinitrc ~/
-mv config/init.vim ~/.config/nvim/
-mv config/alacritty.toml ~/.config/alacritty
-mv config/.zshrc ~/
+mv .config ~/
+mv .zshrc ~/
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
