7652c6619733c161319915179d8c8add02db5d68
killertofus <cinemassacres@gmail.com>
2024-07-07 15:08:14 -0600

    you're toxic

--- dwml.sh
+++ dwml.sh
@@ -91,7 +91,7 @@ sudo mv  rustdesk Chatterino Streamlink_Twitch_GUI /usr/local/bin
 wget -qO - https://apt.packages.shiftkey.dev/gpg.key | gpg --dearmor | sudo tee /usr/share/keyrings/shiftkey-packages.gpg > /dev/null
 sudo sh -c 'echo "deb [arch=amd64 signed-by=/usr/share/keyrings/shiftkey-packages.gpg] https://apt.packages.shiftkey.dev/ubuntu/ any main" > /etc/apt/sources.list.d/shiftkey-packages.list'
 sudo apt update
-sudo apt install github-desktop
+sudo apt install github-desktop -y
 sudo apt update -y && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
 ./gwml.sh
 ./Jdownloader2.sh
