ec661e236c6f9ec8ceee1689e393b69d58f685ec
killertofus <cinemassacres@gmail.com>
2024-07-07 12:42:10 -0600

    other branches????

--- gwml.sh
+++ gwml.sh
@@ -1,30 +1,10 @@
 #!/usr/bin/env sh
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-wget https://github.com/dracula/qbittorrent/raw/master/dracula.qbtheme
 echo "RANGER_LOAD_DEFAULT_RC=false"
 mkdir -p ~/.themes
-mv dracula.qbtheme ~/.themes
-git clone https://github.com/dracula/gtk
-mv gtk/gtk-4.0 ~/.themes
-mkdir -p ~/.config/nvim
-mkdir -p ~/.config/alacritty
-mkdir -p ~/.config/dwm
-mkdir -p ~/.config/rofi
-mkdir -p ~/.config/picom
-mkdir -p ~/.config/ranger
-mkdir -p ~/.config/tmux
-mv wallpaper.jpg ~/.config/dwm/wallpaper.jpg
-sudo mv config/dwm.desktop /usr/share/xsessions/
-mv config/.xinitrc ~/
-mv config/init.vim ~/.config/nvim/
-mv config/alacritty.toml ~/.config/alacritty
-mv config/dwm/* ~/.config/dwm
-mv config/config.rasi ~/.config/rofi
-mv config/picom.conf ~/.config/picom
-mv config/tmux.conf ~/.config/tmux
-mv config/.zshrc ~/
-mv config/plugins config/rc.conf ~/.config/ranger
-mv config/.dircolors ~/
+mv .themes ~/
+mv .config ~/
+mv .zshrc ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
