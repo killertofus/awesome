68a19ab86f671177460bfb6be160af171b08910c
killertofus <cinemassacres@gmail.com>
2024-07-10 22:07:55 +0000

    Update dwml.sh

--- dwml.sh
+++ dwml.sh
@@ -95,7 +95,7 @@ sudo apt install github-desktop -y
 sudo apt update -y && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove
 ./gwml.sh
 ./Jdownloader2.sh
-nvim &
+nvim > /dev/null 2>&1 & 
 git clone --recurse-submodules https://github.com/fairyglade/ly
 cd ly
 zig build
