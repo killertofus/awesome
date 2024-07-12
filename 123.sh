ec6e97eca39064c3a48422d754cf3e6f79d71319
killertofus <cinemassacres@gmail.com>
2024-07-12 18:08:33 +0000

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -1,11 +1,16 @@
-#!/bin/bash
-echo " dwl or dwm"
-echo "  1)dwl"
-echo "  2)dwm"
-
-read n
-case $n in
-  1) echo "sudo apt install wayland-protocols libwayland-cursor++1 libwayland-bin libinput-devlib wayland-dev libwlroots-dev ";;
-  2) echo "dwm";;
-  *) echo "invalid option";;
-esac
+PS3='dwl or dwm: '
+options=("dwl" "dwm")
+select opt in "${options[@]}"
+do
+    case $opt in
+        "dwl")
+            sudo apt install wayland-protocols libwayland-cursor++1 libwayland-bin libinput-devlib wayland-dev libwlroots-dev
+            break
+            ;;
+        "dwm")
+            echo "you chose choice 2"
+            break
+            ;;
+        *) echo "invalid option $REPLY";;
+    esac
+done
