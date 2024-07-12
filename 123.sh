4ad5774b9b4aab038e9d33a6f7a07537295c37f1
killertofus <cinemassacres@gmail.com>
2024-07-12 23:09:30 +0000

    Update 123.sh

--- 123.sh
+++ 123.sh
@@ -4,11 +4,12 @@ select opt in "${options[@]}"
 do
     case $opt in
         "dwl")
-            sudo apt install wayland-protocols libwayland-cursor++1 libwayland-bin libinput-dev libwayland-dev libwlroots-dev
+            sudo apt install 
             sed -i 's/dwm/dwl/g' gwml.sh
             sed -i '21d;20d' gwml.sh
             sed -i 's/picom/wdisplays/g' dwml.sh
-            sed -i 's/feh//g' dwml.sh
+            sed -i 's/feh/wayland-protocols/g' dwml.sh
+            ssed '$ libwayland-cursor++1 libwayland-bin libinput-dev libwayland-dev libwlroots-dev' dwmlpkgs.txt
             sed -i 's/volumeicon-alsa/waybar/g' dwml.sh 
             sed -i 's/72/git clone https://github.com/LGFae/swww/g' dwml.sh
             sed -i 's/73/cd sww && cargo build --release/g' dwml.sh 
