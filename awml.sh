d32b775662d08fd783067cbe8f6bcbcc7bd60b5b
killertofus <cinemassacres@gmail.com>
2024-07-24 01:15:27 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -9,6 +9,7 @@ sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
 sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
 sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
+sudo -v
 for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
 sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1)
 sudo systemctl enable ly
