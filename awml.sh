2c963d944143b3f72948e91e52dc717684f34abb
killertofus <cinemassacres@gmail.com>
2024-07-24 23:35:52 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
-sudo pacman -S --needed git base-devel
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si
-cd -
 sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
 sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
-sudo sed -i '32 a IloveCandy' /etc/pacman.conf
+sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
-for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
-sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1)
-sudo systemctl enable ly
-yay --devel --save
-sudo pacman -Syu
-sudo pacman -Qttdq | sudo pacman -Rns -
-yay -Scc 
-sudo systemctl enable --now piavpn.service
+sudo pacman -S --needed git base-devel reflector
 sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syu --noconfirm
+sudo -v
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd -
+sudo -v
+sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1)
+for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
+yay --devel --save
+sudo pacman -Qttdq | sudo pacman -Rns -
+sudo systemctl enable ly
+sudo systemctl enable --now piavpn.service
+yay -Scc 
 chsh -s $(which zsh)
 fc-cache -f -v
 ./gwml.sh
