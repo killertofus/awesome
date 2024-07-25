3f3d2faa1794fc3a03e12f85e3df9e6e44441133
killertofus <cinemassacres@gmail.com>
2024-07-25 17:11:40 -0600

    fdisk -l

---
+++ awml_wayland.sh
@@ -0,0 +1,31 @@
+#!/usr/bin/env bash
+sed -i 's/dwm/dwl/g' gwml.sh
+sed -i 's/xsessions/wayland-sessions/g' gwml.sh
+sed -i 's/.xinitrc/startup.sh/g' gwml.sh
+sed -i '21d;22d' gwml.sh
+sed -i 's/feh/wayland/g' awmlpkgs.txt
+sed -i 's/xscreensaver/wlroots/g' awmlpkgs.txt
+sed -i 's/volumeicon/waybar/g' awmlpkgs.txt
+sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
+sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
+sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
+sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
+sudo pacman -S --needed git base-devel reflector --noconfirm
+sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+sudo pacman -Syu --noconfirm
+sudo -v
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si --noconfirm
+cd -
+sudo -v
+sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1) --noconfirm
+for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
+yay --devel --save
+sudo pacman -Qttdq | sudo pacman -Rns - --noconfirm
+sudo systemctl enable ly
+sudo systemctl enable --now piavpn.service
+yay -Scc 
+chsh -s $(which zsh)
+fc-cache -f -v
+./gwml.sh
