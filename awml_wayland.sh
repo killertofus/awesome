d40d5a34a9d3ff172ff808e06a30e68cf8ed37bd
killertofus <cinemassacres@gmail.com>
2024-07-25 23:48:26 +0000

    Update awml_wayland.sh

--- awml_wayland.sh
+++ awml_wayland.sh
@@ -5,6 +5,7 @@ sed -i 's/.xinitrc/startup.sh/g' gwml.sh
 sed -i '21d;22d' gwml.sh
 sed -i 's/feh/wayland/g' awmlpkgs.txt
 sed -i 's/xscreensaver/wlroots/g' awmlpkgs.txt
+sed -i 's/picom/swww/g' awmlpkgs.txt
 sed -i 's/volumeicon/waybar/g' awmlpkgs.txt
 sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
 sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
@@ -25,6 +26,8 @@ yay --devel --save
 sudo pacman -Qttdq | sudo pacman -Rns - --noconfirm
 sudo systemctl enable ly
 sudo systemctl enable --now piavpn.service
+swww-daemon
+swww img ~/.config/dwl/wallpaper.jpg
 yay -Scc 
 chsh -s $(which zsh)
 fc-cache -f -v
