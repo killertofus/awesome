9f830c81d5f0918d447623de3cb5d5841f70d742
killertofus <cinemassacres@gmail.com>
2024-07-26 03:08:29 +0000

    s.nail

--- awml_wayland.sh
+++ awml_wayland.sh
@@ -11,6 +11,7 @@ sudo sed -i '/ParallelDownloads/s/^#//g' /etc/pacman.conf
 sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
 sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
+sudo mkdir /usr/share/wayland-sessions/
 sudo pacman -S --needed git base-devel reflector --noconfirm
 sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syu --noconfirm
