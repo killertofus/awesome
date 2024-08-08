bc852d314738793e3a1d3f110d65daa7edcd6123
killertofus <cinemassacres@gmail.com>
2024-08-08 23:50:09 +0000

    Update awml.sh

--- awml.sh
+++ awml.sh
@@ -4,6 +4,7 @@ sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
 sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i 's/debug/!debug/g' /etc/makepkg.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
+sudo --validate
 sudo pacman -S --needed git base-devel reflector --noconfirm
 sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
 sudo pacman -Syu --noconfirm
