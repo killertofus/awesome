0cc0905359bcb9d6c4184c0bb8480c18455e0ebd
killertofus <cinemassacres@gmail.com>
2024-08-14 22:41:57 +0000

    riddin spinnaz

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,10 +1,10 @@
 # please don't run this more then twice :)
 pkgToRemoveListFull=""
-pkgToRemoveList="nano *pop-* *printer-driver* *thunderbird* *cosmic-* *libreoffice-* man-db manpages  *language-pack* *hunspell* cups-common xterm"
+pkgToRemoveList="nano popsicle *pop-* *printer-driver* *thunderbird* *cosmic-* *libreoffice-* man-db manpages *gnome* *language-pack* *hunspell* cups-common xterm"
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(apt --status $pkgToRemove &> /dev/null)
   if [[ $? -eq 0 ]]; then
     pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
   fi
 done
-sudo apt purge $pkgToRemoveList --allow-remove-essential -y
+sudo apt purge $pkgToRemoveList --allow-remove-essential -y && sudo apt install gnome-disk-utility -y
