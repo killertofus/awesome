f46fc99aa5350d4215116a73a9bd2934e35ba5c7
killertofus <cinemassacres@gmail.com>
2024-08-14 20:59:36 +0000

    fucking thing sucks

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,6 +1,6 @@
 # please don't run this more then twice :)
-pkgToRemoveListFull="nano *printer-driver* *thunderbird* *libreoffice* *cosmic-* **libreoffice-* man-db manpages *language-pack* *hunspell* cups-common xterm"
-pkgToRemoveList=""
+pkgToRemoveListFull=""
+pkgToRemoveList="nano *printer-driver* *thunderbird* *cosmic-* *libreoffice-* man-db manpages *language-pack* *hunspell* cups-common xterm"
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(apt --status $pkgToRemove &> /dev/null)
   if [[ $? -eq 0 ]]; then
