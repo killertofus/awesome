b980735fdb3f46617054719b2b49576c25e69161
killertofus <cinemassacres@gmail.com>
2024-08-14 20:25:23 +0000

    Update pwmlrmvpkgs.sh

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -1,6 +1,6 @@
 # please don't run this more then twice :)
-pkgToRemoveListFull=" nano *printer-driver* *thunderbird* *libreoffice* *cosmic-* *gnome* gnome-orca *libreoffice-* man-db manpages *lamguage-pack* *hunspell* cups-common xterm "
-pkgToRemoveList=""
+pkgToRemoveListFull=""
+pkgToRemoveList="nano *printer-driver* *thunderbird* *libreoffice* *cosmic-* *gnome* gnome-orca *libreoffice-* man-db manpages *lamguage-pack* *hunspell* cups-common xterm"
 for pkgToRemove in $(echo $pkgToRemoveListFull); do
   $(dpkg --status $pkgToRemove &> /dev/null)
   if [[ $? -eq 0 ]]; then
