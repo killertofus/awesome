0f19620860a0b80c62f4b0a0801c97d93e68843e
killertofus <cinemassacres@gmail.com>
2024-08-15 18:28:07 +0000

    test ;0

--- pwmlrmvpkgs.sh
+++ pwmlrmvpkgs.sh
@@ -7,4 +7,4 @@ for pkgToRemove in $(echo $pkgToRemoveListFull); do
     pkgToRemoveList="$pkgToRemoveList $pkgToRemove"
   fi
 done
-sudo apt purge $pkgToRemoveList --allow-remove-essential -y && sudo apt install policykit-1-gnome gnome-disk-utility -y
+sudo apt purge $pkgToRemoveList --allow-remove-essential -y && sudo apt install policykit-1-gnome gnome-disk-utility -y && sudo apt update && sudo apt upgrade && sudo apt clean && sudo apt autoclean && sudo apt autoremove -y
