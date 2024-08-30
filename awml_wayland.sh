53d7f240e0a7598193a16a6992d58c0d0cb019d0
killertofus <cinemassacres@gmail.com>
2024-08-30 22:13:39 +0000

    the joker stole this commit

--- awml_wayland.sh
+++ awml_wayland.sh
@@ -15,16 +15,19 @@ sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
 sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
 sudo mkdir -p /usr/share/wayland-sessions
+sudo -v
 sudo pacman -S --needed git base-devel reflector --noconfirm
 sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
+sudo -v
 sudo pacman -Syu --noconfirm
 sudo -v
+sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1) --noconfirm
+fc-cache -f -v
+chsh -s $(which zsh)
 git clone https://aur.archlinux.org/yay.git
 cd yay
 makepkg -si --noconfirm
 cd -
-sudo -v
-sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1) --noconfirm
 for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
 yay --devel --save
 sudo pacman -Qttdq | sudo pacman -Rns - --noconfirm
@@ -32,9 +35,6 @@ sudo systemctl enable ly
 sudo systemctl enable piavpn.service
 sudo systemctl enable libvirtd.service
 sudo systemctl enable libvirtd.socket
+yay -Scc --noconfirm
 nvim > /dev/null 2>&1 &
-swww img ~/.config/dwl/wallpaper.jpg
-yay -Scc 
-chsh -s $(which zsh)
-fc-cache -f -v
 ./gwml.sh
