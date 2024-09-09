568483e235a08a246e7328a77da62ee02cf10a37
killertofus <cinemassacres@gmail.com>
2024-09-09 03:31:47 +0000

    f is for fuck

--- gwml.sh
+++ gwml.sh
@@ -15,3 +15,65 @@ unzip master.zip
 mv lib dracula.zsh-theme ~/oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 osascript -e 'tell application "Finder" to set desktop picture to POSIX file "~/Pictures/wallpaper.jpg"'
+# Disable personalized advertisements and identifier tracking
+echo '--- Disable personalized advertisements and identifier tracking'
+defaults write com.apple.AdLib allowIdentifierForAdvertising -bool false
+defaults write com.apple.AdLib allowApplePersonalizedAdvertising -bool false
+defaults write com.apple.AdLib forceLimitAdTracking -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ---------Disable guest sign-in from login screen----------
+# ----------------------------------------------------------
+echo '--- Disable guest sign-in from login screen'
+sudo defaults write /Library/Preferences/com.apple.loginwindow GuestEnabled -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disable guest access to file shares over AF--------
+# ----------------------------------------------------------
+echo '--- Disable guest access to file shares over AF'
+sudo defaults write /Library/Preferences/SystemConfiguration/com.apple.smb.server AllowGuestAccess -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disable guest access to file shares over SMB-------
+# ----------------------------------------------------------
+echo '--- Disable guest access to file shares over SMB'
+sudo defaults write /Library/Preferences/com.apple.AppleFileServer guestAccess -bool NO
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------Disable incoming SSH and SFTP remote logins--------
+# ----------------------------------------------------------
+echo '--- Disable incoming SSH and SFTP remote logins'
+echo 'yes' | sudo systemsetup -setremotelogin off
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ------------Disable the insecure TFTP service-------------
+# ----------------------------------------------------------
+echo '--- Disable the insecure TFTP service'
+sudo launchctl disable 'system/com.apple.tftpd'
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# ----------Disable Bonjour multicast advertising-----------
+# ----------------------------------------------------------
+echo '--- Disable Bonjour multicast advertising'
+sudo defaults write /Library/Preferences/com.apple.mDNSResponder.plist NoMulticastAdvertisements -bool true
+# ----------------------------------------------------------
+
+
+# ----------------------------------------------------------
+# -------------Disable insecure telnet protocol-------------
+# ----------------------------------------------------------
+echo '--- Disable insecure telnet protocol'
+sudo launchctl disable system/com.apple.telnetd
+# ----------------------------------------------------------
