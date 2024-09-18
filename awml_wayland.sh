ccb432e6b59c04b748e2deca15e9b212b8608a45
killertofus <cinemassacres@gmail.com>
2024-09-18 17:50:28 +0000

    this commit blew me down

--- awml_wayland.sh
+++ awml_wayland.sh
@@ -16,19 +16,15 @@ sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
 sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
 EXPORT PKG_CONFIG_PATH=/usr/lib/wlroots0.17/pkgconfig
 sudo mkdir -p /usr/share/wayland-sessions
+git clone https://aur.archlinux.org/yay.git
+makepkg -siD yay --noconfirm
 sudo -v
-sudo pacman -S --needed git base-devel reflector --noconfirm
-sudo reflector --latest 200 --sort rate --save /etc/pacman.d/mirrorlist
-sudo -v
+sudo pacman -S --needed git base-devel --noconfirm
 sudo pacman -Syu --noconfirm
 sudo -v
 sudo pacman -S $(cat awmlpkgs.txt | cut -d' ' -f1) --noconfirm
 fc-cache -f -v
 chsh -s $(which zsh)
-git clone https://aur.archlinux.org/yay.git
-cd yay
-makepkg -si --noconfirm
-cd -
 for word in $(cat ywmlpkgs.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
 yay --devel --save
 sudo pacman -Qttdq | sudo pacman -Rns - --noconfirm
