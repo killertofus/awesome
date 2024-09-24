2f3a3502aed1f6e56a4d31dbe90250dffaf50fd0
killertofus <cinemassacres@gmail.com>
2024-09-24 19:28:36 +0000

    you must be under 100 but over "7" to see this commit

--- gwml.sh
+++ gwml.sh
@@ -1,5 +1,4 @@
 #!/usr/bin/env sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 wget https://github.com/dracula/qbittorrent/raw/master/dracula.qbtheme
 echo "RANGER_LOAD_DEFAULT_RC=false"
 mv .themes ~/
