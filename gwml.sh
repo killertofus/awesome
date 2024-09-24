a89b4d5a1c38d2e0609355882cd1d43f14aad428
killertofus <cinemassacres@gmail.com>
2024-09-24 19:40:14 +0000

    I did it again

--- gwml.sh
+++ gwml.sh
@@ -9,7 +9,6 @@ mv .dircolors ~/
 sudo mv awesome.desktop /usr/share/xsessions/
 mv .xinitrc ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
