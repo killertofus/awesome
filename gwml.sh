63e574f7a04fc3b017abc829d8d3df1a42c3472f
killertofus <cinemassacres@gmail.com>
2024-09-24 19:41:15 +0000

    this commit was mad in doom

--- gwml.sh
+++ gwml.sh
@@ -2,13 +2,12 @@
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 echo "RANGER_LOAD_DEFAULT_RC=false"
 mv .themes ~/
+cp -r .config ~/
+mv .zshrc ~/
 mv .icons ~/
 mv .dircolors ~/
-mv .cursors ~/
-mv .zshrc ~/
-mv .xinitrc ~/
 sudo mv awesome.desktop /usr/share/xsessions/
-cp -r .config ~/
+mv .xinitrc ~/
 git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
