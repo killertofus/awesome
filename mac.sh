a008f921e4c4753231bdb7ffcad625a43f7d4b92
killertofus <cinemassacres@gmail.com>
2024-09-24 19:04:20 +0000

    did you forget about this commit?

--- mac.sh
+++ mac.sh
@@ -1,7 +1,5 @@
 #!/usr/bin/env sh
-NONINTERACTIVE=1 /bin/bash -c \
-   "$(curl -fsSL \
-      https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew install < macpackages.txt
 pip install --user libtmux
 mv .zshrc ~/
