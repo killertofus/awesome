1a1033f7fddcd4c544383b354f5271b03599d285
killertofus <cinemassacres@gmail.com>
2024-09-28 02:34:34 +0000

    you chose down

--- gwml.sh
+++ gwml.sh
@@ -5,8 +5,8 @@ mv .themes ~/
 cp -r .config ~/
 mv .zshrc ~/
 mv .dircolors ~/
-git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
-
+git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
+~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
