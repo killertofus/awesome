146c0f0a8f65767371929041ea9409a0bb56f757
killertofus <cinemassacres@gmail.com>
2024-09-28 04:19:06 +0000

    this commit best viewed on 5d

--- gwml.sh
+++ gwml.sh
@@ -5,6 +5,7 @@ mv .icons ~/
 git clone https://github.com/dracula/gtk
 mv .config ~/
 mv .zshrc ~/
+gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
