df1688d977889260ffae0f87ac1a704cace0d9cc
killertofus <cinemassacres@gmail.com>
2024-10-07 15:49:30 -0600

    this commit got crunk

--- mac.sh
+++ mac.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env sh
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
+brew tap zegervdv/zathura
 brew install < macpackages.txt
 pip install --user libtmux
+mkdir -p $(brew --prefix zathura)/lib/zathura
+ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
 mv .zshrc ~/
 nvim > /dev/null 2>&1 &
 mv wallpaper.jpg ~/Pictures
