aae050acdc0e4393a40a473c5c903df1964b1ce9
killertofus <cinemassacres@gmail.com>
2024-10-09 11:30:42 -0600

    XDDDD

--- windows.ps1
+++ windows.ps1
@@ -1,5 +1,6 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install packages.config -y
 move wallpaper.jpg %USERPROFILE%/Pictures
+move config/nvim ~/AppData/Local/nvim
 irm https://christitus.com/win | iex
 wget -r -l 1 --span-hosts --accept-regex=.*bmrf.org/repos/tron//.*.exe -erobots=off -nH https://bmrf.org/repos/tron/
