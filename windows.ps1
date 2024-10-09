448fab0a9b1ec3b2e1cb2f1962e3e454f71a1ce2
killertofus <cinemassacres@gmail.com>
2024-10-09 12:59:36 -0600

    WHAT IS LOVE

--- windows.ps1
+++ windows.ps1
@@ -1,6 +1,10 @@
 Set-ExecutionPolicy Bypass -Scope Process -Force; [System.Net.ServicePointManager]::SecurityProtocol = [System.Net.ServicePointManager]::SecurityProtocol -bor 3072; iex ((New-Object System.Net.WebClient).DownloadString('https://community.chocolatey.org/install.ps1'))
 choco install packages.config -y
+Enable-WindowsOptionalFeature -Online -FeatureName Microsoft-Windows-Subsystem-Linux
+Enable-WindowsOptionalFeature -Online -FeatureName VirtualMachinePlatform
+wsl --set-default-version 2
 move wallpaper.jpg %USERPROFILE%/Pictures
 move config/nvim ~/AppData/Local/nvim
+move config/vlc ~/%appdata%/ 
 irm https://christitus.com/win | iex
 wget -r -l 1 --span-hosts --accept-regex=.*bmrf.org/repos/tron//.*.exe -erobots=off -nH https://bmrf.org/repos/tron/
