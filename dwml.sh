3e376dd4e85df525b26ab8ffafe0b3bc7952b9b2
killertofus <cinemassacres@gmail.com>
2024-10-13 13:07:06 -0600

    Sell the kids for food

--- dwml.sh
+++ dwml.sh
@@ -4,6 +4,7 @@ sudo wget -p /usr/share/fonts/Iosevka
 sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
 sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevka/
 sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
+sudo apt update
 xargs sudo apt install < dwmlpkgs.txt -y
 sudo mv update.sh /usr/local/bin
 chsh -s $(which zsh)
