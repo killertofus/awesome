da12d37c48c75235f613ada3cf8786300a492cbb
killertofus <cinemassacres@gmail.com>
2024-10-22 13:44:19 -0600

    I'm afraid you're going to have to do better commit

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ sudo wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBr
 sudo tar -xvf /usr/share/fonts/JetBrains/JetBrainsMono.tar.xz -C /usr/share/fonts/JetBrains/
 sudo rm -rf /usr/share/fonts/JetBrains/JetBrainsMono.tar.xz /usr/share/fonts/JetBrains/*.md
 sudo wget -p /usr/share/fonts/Iosevka
-sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
 sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevka/
 sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
 sudo apt update
