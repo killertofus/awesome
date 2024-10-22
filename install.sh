0755043a4c67a88e7d18ebb1856284c38be4787b
killertofus <cinemassacres@gmail.com>
2024-10-22 13:46:01 -0600

    Find a way

--- install.sh
+++ install.sh
@@ -1,11 +1,11 @@
 #!/usr/bin/env bash
 sudo wget -p /usr/share/fonts/JetBrains
-sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz  --directory-prefix=/usr/share/fonts/JetBrains
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/JetBrainsMono.tar.xz  --directory-prefix=/usr/share/fonts/JetBrains
 sudo tar -xvf /usr/share/fonts/JetBrains/JetBrainsMono.tar.xz -C /usr/share/fonts/JetBrains/
 sudo rm -rf /usr/share/fonts/JetBrains/JetBrainsMono.tar.xz /usr/share/fonts/JetBrains/*.md
 sudo wget -p /usr/share/fonts/Iosevka
 sudo wget -p /usr/share/fonts/Iosevka
-sudo wget  https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
+sudo wget https://github.com/ryanoasis/nerd-fonts/releases/latest/download/Iosevka.tar.xz --directory-prefix=/usr/share/fonts/Iosevka/
 sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevka/
 sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
 sudo apt update
