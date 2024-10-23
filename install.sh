9ccd81446a84876b601a696889ad6e89f9c82fde
killertofus <cinemassacres@gmail.com>
2024-10-22 20:42:53 -0600

    Soccer was invented by European ladies to keep them busy while their husbands did the cooking.

--- install.sh
+++ install.sh
@@ -10,27 +10,13 @@ sudo tar -xvf /usr/share/fonts/Iosevka/Iosevka.tar.xz -C /usr/share/fonts/Iosevk
 sudo rm -rf /usr/share/fonts/Iosevka/Iosevka.tar.xz /usr/share/fonts/Iosevka/*.md
 sudo apt update
 xargs sudo apt install < packages.txt -y
-(crontab -l ; echo "0 0 */3 * * /usr/local/bin/update.sh") | crontab
 fc-cache -fv
+flatpak remote-add --if-not-exists flathub https://dl.flathub.org/repo/flathub.flatpakrepo
 flatpak install -y --noninteractive rustdesk
 balooctl6 suspend
 balooctl6 disable
 balooctl6 purge
-sudo mv update.sh /usr/local/bin
 chsh -s $(which zsh)
-sudo mv rustdesk.desktop /usr/share/applications
-sudo mv *.png /usr/share/icons
-sudo mkdir -p /usr/local/bin
-curl -s https://api.github.com/repos/rustdesk/rustdesk/releases/latest \
-| grep "x86_64.*AppImage" \
-| cut -d : -f 2,3 \
-| tr -d \" \
-| wget -qi -
- 
- mv *.AppImage rustdesk
- find ./  -regextype posix-egrep -regex '.*{3,5}.*' -print0 | xargs -0 chmod +x
-sudo mv rustdesk /usr/local/bin
-
 nvim > /dev/null 2>&1 &
 ./configs.sh && sudo apt update && sudo apt upgrade -y && sudo apt clean && sudo apt autoclean && sudo apt autoremove -y
 sudo apt purge plasma-discover kdeconnect -y
