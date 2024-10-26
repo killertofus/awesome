92cf1d30b1fbe3547fb26c3bd2d975ebf73f7809
killertofus <cinemassacres@gmail.com>
2024-10-26 12:05:55 -0600

    I'm planning on dying tonight. What are *your* plans?

--- install.sh
+++ install.sh
@@ -12,7 +12,7 @@ sudo pacman -S --needed git base-devel --noconfirm
 sudo pacman -Syu --noconfirm
 sudo -v
 sudo pacman -S $(cat packages.txt | cut -d' ' -f1) --noconfirm
-fc-cache -fv
+fc-cache -f
 chsh -s $(which zsh)
 for word in $(cat aurpackages.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
 yay --devel --save
