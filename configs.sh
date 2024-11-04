d59ec5381aaa006b750332541a0e6a5cd8f1b094
killertofus <cinemassacres@gmail.com>
2024-11-03 18:56:35 -0700

    In fact, this commit  has been dead for 130 years.

--- configs.sh
+++ configs.sh
@@ -7,8 +7,6 @@ mv icons ~/.local/share
 mv .dircolors ~/
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 gsettings set org.nemo.preferences show-hidden-files-true
-sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
-       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
