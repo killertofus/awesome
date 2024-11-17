1396156e2be13b83d51b00623fc198e1fa13f996
killertofus <cinemassacres@gmail.com>
2024-11-16 23:39:42 -0700

    fuck

--- .config/dwl/config.mk
+++ .config/dwl/config.mk
@@ -11,7 +11,7 @@ DATADIR = $(PREFIX)/share
 XWAYLAND =
 XLIBS =
 # Uncomment to build XWayland support
-#XWAYLAND = -DXWAYLAND
-#XLIBS = xcb xcb-icccm
+XWAYLAND = -DXWAYLAND
+XLIBS = xcb xcb-icccm
 
 CC = gcc
