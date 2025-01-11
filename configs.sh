0b4457a2d7f67878eb6016e1053685e9abb4bfc6
killertofus <cinemassacres@gmail.com>
2025-01-10 22:00:09 -0700

    All students please proceed immediately to an assembly in the Butthead Memorial Auditorium.

--- configs.sh
+++ configs.sh
@@ -1,15 +1,15 @@
 #!/usr/bin/env sh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 mv themes ~/.local/share
-cp -r .config ~/
 mv .zshrc ~/
+cp -r .config ~/
 mv icons ~/.local/share
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
-gsettings set org.nemo.preferences show-hidden-files-true
+gsettings set org.nemo.preferences show-hidden-files true
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.oh-my-zsh/themes
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.config/oh-my-zsh/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 export ZSH="$XDG_DATA_HOME"/oh-my-zsh
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 export XCURSOR_PATH=/usr/share/icons:$XDG_DATA_HOME/icons
