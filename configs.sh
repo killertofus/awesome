16a3cb7f8d611c25de9f2ce7657aaab70854bfa8
killertofus <cinemassacres@gmail.com>
2025-01-10 22:04:58 -0700

    I was an astronaut.

--- configs.sh
+++ configs.sh
@@ -1,19 +1,15 @@
 #!/usr/bin/env sh
 ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 mv themes ~/.local/share
-cp -r .config ~/
 mv .zshrc ~/
+cp -r .config ~/
 mv icons ~/.local/share
-sudo mv awesome.desktop /usr/share/xsessions/
-mv .xinitrc ~/
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 gsettings set org.nemo.preferences show-hidden-files true
-git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
 mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.config/oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
-~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 export ZSH="$XDG_DATA_HOME"/oh-my-zsh
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 export XCURSOR_PATH=/usr/share/icons:$XDG_DATA_HOME/icons
