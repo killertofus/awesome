453f2eaae8d204dbce51f907cdf16a7db5b7a226
killertofus <cinemassacres@gmail.com>
2025-01-10 21:16:29 -0700

    no Rodgers and Hammerstein!

--- configs.sh
+++ configs.sh
@@ -2,9 +2,11 @@
 ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
 mv themes ~/.local/share
 cp -r .config ~/
+cp -r icons ~/.local/share
 mv .zshrc ~/
 mv icons ~/.local/share
-sudo mv awesome.desktop /usr/share/xsessions/
+sudo mkdir -p /usr/share/xsessions
+sudo mv dwm.desktop /usr/share/xsessions/
 mv .xinitrc ~/
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
 gsettings set org.nemo.preferences show-hidden-files true
@@ -14,6 +16,10 @@ unzip master.zip
 mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.config/oh-my-zsh/themes
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
+make -C ~/.config/dwm/ clean
+sudo make -C ~/.config/dwm/ clean install
+make -C ~/.config/dwm/slstatus/ clean
+sudo make -C ~/.config/dwm/slstatus/ clean install
 export ZSH="$XDG_DATA_HOME"/oh-my-zsh
 export WINEPREFIX="$XDG_DATA_HOME"/wine
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
