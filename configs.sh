808c8cee47b3e1b8710e75b134aeefdc0a83b86d
killertofus <cinemassacres@gmail.com>
2025-01-11 16:51:47 -0700

    test

--- configs.sh
+++ configs.sh
@@ -1,5 +1,6 @@
 #!/usr/bin/env sh
-ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv ~/.oh-my-zsh ~/.local/share
 mv themes ~/.local/share
 cp -r .config ~/
 mv .zshrc ~/
