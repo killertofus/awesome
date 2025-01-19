036c2a8a513534637458499442da8a6bb0742e77
killertofus <cinemassacres@gmail.com>
2025-01-18 18:29:41 -0700

    And the crackers kept on comin'.

--- configs.sh
+++ configs.sh
@@ -2,10 +2,10 @@
 export XDG_DATA_HOME=".local/share/"
 export ZSH="$HOME/.local/share/oh-my-zsh"
 sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mv themes ~/.local/share
+mv themes "$XDG_DATA_HOME"
 cp -r .config ~/
 mv .zshrc ~/
-mv icons ~/.local/share
+mv icons "$XDG_DATA_HOME"
 sudo mv awesome.desktop /usr/share/xsessions/
 mv .xinitrc ~/
 gsettings set org.cinnamon.desktop.privacy remember-recent-files false
@@ -13,8 +13,8 @@ gsettings set org.nemo.preferences show-hidden-files true
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.local/share/oh-my-zsh/themes
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.local/share/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv zsh-master/lib zsh-master/dracula.zsh-theme ~/"$XDG_DATA_HOME"/share/oh-my-zsh/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-"$XDG_DATA_HOME"/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 export WINEPREFIX="$XDG_DATA_HOME"/wine
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
