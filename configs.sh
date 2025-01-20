1ae9b5e4f4f2d4295025dc381e0e53d4b15dbfed
killertofus <cinemassacres@gmail.com>
2025-01-20 11:43:52 -0700

    If this world were mine

--- configs.sh
+++ configs.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env sh
-ZSH="$XDG_CONFIG_HOME/oh-my-zsh" sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
-mv themes ~/.local/share
+export XDG_DATA_HOME=~/.local/share
+export ZSH="$XDG_DATA_HOME/oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)" "" --unattended
+mv themes "$XDG_DATA_HOME"
 cp -r .config ~/
-cp -r icons ~/.local/share
 mv .zshrc ~/
-mv icons ~/.local/share
+mv icons "$XDG_DATA_HOME"
 sudo mkdir -p /usr/share/xsessions
 sudo mv dwm.desktop /usr/share/xsessions/
 mv .xinitrc ~/
@@ -13,14 +14,13 @@ gsettings set org.nemo.preferences show-hidden-files true
 git clone https://github.com/tmux-plugins/tpm ~/.config/tmux/plugins/tpm
 wget https://github.com/dracula/zsh/archive/master.zip
 unzip master.zip
-mv zsh-master/lib zsh-master/dracula.zsh-theme ~/.config/oh-my-zsh/themes
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-autosuggestions
+mv zsh-master/lib zsh-master/dracula.zsh-theme "$XDG_DATA_HOME"/oh-my-zsh/themes
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-"$XDG_DATA_HOME"/oh-my-zsh/custom}/plugins/zsh-autosuggestions
 ~/.config/tmux/plugins/tpm/scripts/install_plugins.sh
 make -C ~/.config/dwm/ clean
 sudo make -C ~/.config/dwm/ clean install
 make -C ~/.config/dwm/slstatus/ clean
 sudo make -C ~/.config/dwm/slstatus/ clean install
-export ZSH="$XDG_DATA_HOME"/oh-my-zsh
 export WINEPREFIX="$XDG_DATA_HOME"/wine
 export HISTFILE="$XDG_STATE_HOME"/zsh/history
 export XCURSOR_PATH=/usr/share/icons:$XDG_DATA_HOME/icons
