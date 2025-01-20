07f0514e3148f7c352ca4837a81c18589d50b31a
killertofus <cinemassacres@gmail.com>
2025-01-20 12:49:36 -0700

    Aw, what kind of horrible suicide-free time is this?

--- mac.sh
+++ mac.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env sh
 NONINTERACTIVE=1 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 brew tap zegervdv/zathura
-brew install < macpackages.txt
+brew install < packages.txt
 pip install --user libtmux
 mkdir -p $(brew --prefix zathura)/lib/zathura
 ln -s $(brew --prefix zathura-pdf-poppler)/libpdf-poppler.dylib $(brew --prefix zathura)/lib/zathura/libpdf-poppler.dylib
