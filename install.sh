4ebf02b7e35592a61829e100c04b21c897a9fdb5
killertofus <cinemassacres@gmail.com>
2025-02-14 22:20:16 -0700

    thanks nick
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=995670#193

--- install.sh
+++ install.sh
@@ -34,18 +34,6 @@ sudo mv streamlink.desktop /usr/share/applications
 sudo mv *.png /usr/share/icons
 sudo mkdir -p /usr/local/bin
 
-
-mkdir zig
-cd zig
-wget https://ziglang.org/download/0.13.0/zig-linux-x86_64-0.13.0.tar.xz
- tar xf *
- rm -rf *.tar.xz
- mv * zig
- sudo mv zig/lib zig/zig /usr/local/bin
- cd -
-
-sudo -v
-
 git clone https://github.com/sxyazi/yazi.git
 cargo build --release --locked --manifest-path=yazi/Cargo.toml
 sudo mv yazi/target/release/yazi yazi/target/release/ya /usr/local/bin
