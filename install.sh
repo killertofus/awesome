489d70096dcb6820b21839c80f21a9f130e80ab2
killertofus <cinemassacres@gmail.com>
2025-03-16 23:31:37 -0600

    thanks Jongmin
    https://bugs.debian.org/cgi-bin/bugreport.cgi?bug=908894#36

--- install.sh
+++ install.sh
@@ -37,12 +37,6 @@ git clone https://github.com/sxyazi/yazi.git
 cargo build --release --locked --manifest-path=yazi/Cargo.toml
 sudo mv yazi/target/release/yazi yazi/target/release/ya /usr/local/bin
 
-LAZYGIT_VERSION=$(curl -s "https://api.github.com/repos/jesseduffield/lazygit/releases/latest" | grep -Po '"tag_name": "v\K[^"]*')
-curl -Lo lazygit.tar.gz "https://github.com/jesseduffield/lazygit/releases/latest/download/lazygit_${LAZYGIT_VERSION}_Linux_x86_64.tar.gz"
-tar xf lazygit.tar.gz lazygit
-sudo install lazygit /usr/local/bin
-
-
 
 flatpak install -y --noninteractive flathub com.chatterino.chatterino/x86_64/stable org.jellyfin.JellyfinServer JDownloader rustdesk
 sudo mv streamlink.desktop /usr/share/applications
