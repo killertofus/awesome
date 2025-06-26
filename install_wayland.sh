67c614a30ecd2bf037450132201b9ddebbb51c09
killertofus <cinemassacres@gmail.com>
2025-06-26 15:42:29 -0600

    Now stop asking silly questions and go kill your brother!

--- install_wayland.sh
+++ install_wayland.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
+sudo zypper up
+sudo zypper addrepo > obslinks.txt
+sudo sed -i 's/#download.max_concurrent_connections/download.max_concurrent_connections/g' /etc/zypp/zypp.conf
+sudo env ZYPP_CURL2=1 zypper ref
+sudo env ZYPP_PCK_PRELOAD=1 zypper dup
+sudo zypper refresh
+sudo systemctl set-default graphical.target
+sed -i 's/dwm/dwl/g' configs.sh
+sed -i '2 a cwc' obspackages.txt
+sudo sed -i 's/debug/!debug/g' /etc/makepkg.conf
+sed -i '19d;20d' configs.sh
 sed -i 's/feh/wayland/g' packages.txt
 sed -i 's/rofi/rofi-wayland/g' packages.txt
-sed -i '3d' packages.txt
-sed -i 's/xscreensaver/wlroots0.18/g' packages.txt
+sed -i 's/xscreensaver/wlroots/g' packages.txt
 sed -i 's/picom/swww/g' packages.txt
 sed -i 's/lxappearance/nwg-look/g' packages.txt
 sed -i '2 a wayland-protocols' packages.txt
-sed -i '2 a cwc' aurpackages.txt
 sed -i 's/volumeicon/waybar/g' packages.txt
 sed -i '4 a xdg-desktop-portal-wlr' packages.txt
 sed -i '3 a xdg-desktop-portal-gtk' packages.txt
-sudo sed -i 's/debug/!debug/g' /etc/makepkg.conf
-sudo sed -i '/#MAKEFLAGS="5"/c\MAKEFLAGS="--jobs=$(nproc)"' /etc/makepkg.conf
-sudo sed -i '32 a ILoveCandy' /etc/pacman.conf
-sudo sed -i '/Color/s/^#//g' /etc/pacman.conf
+sudo zypper install < packages.txt
+sudo zypper install < obs_packages.txt
 sudo -v
-sudo pacman -S --needed git base-devel --noconfirm
-git clone https://aur.archlinux.org/yay.git
-makepkg -siD yay --noconfirm
-sudo pacman -Syu --noconfirm
-sudo pacman -S $(cat packages.txt | cut -d' ' -f1) --noconfirm
 fc-cache -f
 chsh -s $(which zsh)
-for word in $(cat aurpackages.txt); do yay -S --noconfirm --mflags --skipinteg $word || true; done
-yay --devel --save
-sudo pacman -Qttdq | sudo pacman -Rns - --noconfirm
-sudo systemctl enable ly piavpn libvirtd.socket libvirtd.service rustdesk
-yay -Scc --noconfirm
+zypper packages --orphaned | grep @System | cut -d '|' -f3 | xargs echo zypper rm -y
+sudo systemctl enable ly rustdesk libvirtd.service libvirtd.socket
 nvim > /dev/null 2>&1 &
 ./configs.sh
 rm -rf $(pwd)
