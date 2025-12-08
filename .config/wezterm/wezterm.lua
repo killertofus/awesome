8c5761a7f8a095fbf2064603456a61c249ecabff
killertofus <cinemassacres@gmail.com>
2025-12-08 12:18:08 -0700

    yo!

--- .config/wezterm/wezterm.lua
+++ .config/wezterm/wezterm.lua
@@ -18,7 +18,29 @@ config.initial_rows = 28
 config.font_size = 10
 config.color_scheme = 'ayu'
 -- test
-local resurrect = require 'resurrect/config'
-config.keys = merge.all(config.keys, resurrect.keys)
+local resurrect = wezterm.plugin.require("https://github.com/MLFlexer/resurrect.wezterm")
+-- loads the state whenever I create a new workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.created", function(window, path, label)
+  local workspace_state = resurrect.workspace_state
+
+  workspace_state.restore_workspace(resurrect.state_manager.load_state(label, "workspace"), {
+    window = window,
+    relative = true,
+    restore_text = true,
+    on_pane_restore = resurrect.tab_state.default_on_pane_restore,
+  })
+end)
+
+-- Saves the state whenever I select a workspace
+wezterm.on("smart_workspace_switcher.workspace_switcher.selected", function(window, path, label)
+  local workspace_state = resurrect.workspace_state
+  resurrect.state_manager.save_state(workspace_state.get_workspace_state())
+end)
+wezterm.on("gui-startup", resurrect.state_manager.resurrect_on_gui_startup)
+---@param opts? {interval_seconds: 30}
+
+
+
+
 -- Finally, return the configuration to wezterm:
 return config
