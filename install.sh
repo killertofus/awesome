bb3d4a731eb95fd376c385442b38c850caa0e15b
killertofus <cinemassacres@gmail.com>
2026-03-02 17:52:57 -0700

    Now, this is my kind of meeting.

--- install.sh
+++ install.sh
@@ -10,6 +10,8 @@ fc-cache -f
 doas chsh -s $(which zsh)
 doas emerge --depclean
 rc-update del agetty.tty2
+rc-update add dbus default
+rc-update add display-manager default
 doas rc-update add ly piavpn libvirtd rustdesk
 nvim > /dev/null 2>&1 &
 ./configs.sh
